5c6880a35d9ee2c8e851ac193f451279a809d867
NikRuchkin1 <prolive419000@gmail.com>
2020-12-08 23:48:34 +0300

    -

--- src/components/Block/index.js
+++ src/components/Block/index.js
@@ -3,9 +3,8 @@ import classNames from 'classnames';
 
 import './Block.scss';
 
-фыс;
-
 const Block = ({ children, className }) => (
+  <div className={classNames('block', className)}>{children}</div>
   <div className={classNames('block', className)}>{children}</div>
 );
 
