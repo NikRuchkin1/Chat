71035db701e7c7aac816b5efb9125136f67099ef
NikRuchkin1 <prolive419000@gmail.com>
2020-12-10 23:48:43 +0300

    -

--- src/components/Block/index.js
+++ src/components/Block/index.js
@@ -4,7 +4,10 @@ import classNames from 'classnames';
 import './Block.scss';
 
 const Block = ({ children, className }) => (
-  <div className={classNames('block', className)}>{children}</div>
+  <div className={classNames('block', className)}>
+    {children}
+    <div></div>
+  </div>
 );
 
 export default Block;
