863a493745dc764c244288ee65dcf48aedd4aaae
NikRuchkin1 <prolive419000@gmail.com>
2020-12-14 23:57:44 +0300

    -

--- src/modules/LoginForm/Components/LoginForm.jsx
+++ src/modules/LoginForm/Components/LoginForm.jsx
@@ -7,6 +7,7 @@ import { validateField } from 'utils/helpers';
 
 const LoginForm = (props) => {
   const { values, touched, errors, handleChange, handleBlur, handleSubmit } = props;
+  const test;
 
   return (
     <div>
